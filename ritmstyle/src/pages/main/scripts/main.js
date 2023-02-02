503267869954d79df95b2808614fa231e5f578d7
eclairdark <eclaircodedev@gmail.com>
2023-02-02 19:04:13 +0500

    feat: add price and about sections

--- ritmstyle/src/pages/main/scripts/main.js
+++ ritmstyle/src/pages/main/scripts/main.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   function openNavigation(menu) {
     const navigation = document.querySelector('.header__navigation');
     const navigationButton = document.querySelector('.navigation-button');
-    if (menu.classList.contains('active')) {
+    if (menu.classList.contains('active-nav')) {
       navigation.style.transform = 'translateX(0)';
       navigationButton.style.display = 'block';
     } else {
@@ -14,8 +14,63 @@ document.addEventListener('DOMContentLoaded', () => {
   const burger = document.querySelector('.burger-menu');
 
   burger.addEventListener('click', () => {
-    burger.classList.toggle('active');
+    burger.classList.toggle('active-nav');
 
     openNavigation(burger);
   });
+
+  //slider
+
+  const slides = [...document.querySelectorAll('.slide')];
+  const dotsWrapper = document.querySelector('.slider__dots');
+  const dots = document.querySelectorAll('.slider__dot');
+
+  function activeDots(e) {
+    dots.forEach((dot) => {
+      dot.classList.remove('active-dot');
+    });
+
+    const sliderDot = e.target.classList;
+
+    if (e.target === dotsWrapper) {
+      dots[1].classList.add('active-dot');
+      addStylesForSlide(slides[1]);
+    } else {
+      e.target.classList.add('active-dot');
+    }
+
+    switch (sliderDot[1]) {
+      case 'first-dot':
+        addStylesForSlide(slides[0]);
+        break;
+      case 'second-dot':
+        addStylesForSlide(slides[1]);
+        break;
+      case 'third-dot':
+        addStylesForSlide(slides[2]);
+        break;
+    }
+  }
+
+  function addStylesForSlide(slide) {
+    const slideTitle = slide.querySelector('.slide__title');
+    const slideDesc = slide.querySelectorAll('.slide__desc');
+
+    slides.forEach((slide) => {
+      if (slide.classList.contains('active')) {
+        slide.classList.remove('active');
+        slide.querySelector('.slide__title').classList.remove('active-title');
+        slide
+          .querySelectorAll('.slide__desc')
+          .forEach((desc) => desc.classList.remove('active-desc'));
+      } else {
+      }
+    });
+
+    slide.classList.add('active');
+    slideTitle.classList.add('active-title');
+    slideDesc.forEach((desc) => desc.classList.add('active-desc'));
+  }
+
+  dotsWrapper.addEventListener('click', activeDots);
 });
